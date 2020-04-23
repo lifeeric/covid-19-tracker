3f9e7c96e7f4a4cbe8134ef6f02d936acb1df8a0
LIFE <dkrshw65@gmail.com>
2020-04-23 11:34:15 -0400

    port 1

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require('express');
-const path = require('path');
+const path    = require('path');
+
 const app = express();
-
-
-// Route
 
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
-
-// Listen
-const PORT = process.env.PORT | 5000;
-app.listen(PORT, () => console.log(`Server is running`));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log('running'));
