bd6fd355b3ea75305a7be0843ae1404d086a42a4
LIFE <dkrshw65@gmail.com>
2020-04-23 11:26:17 -0400

    fixed

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use('/*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
